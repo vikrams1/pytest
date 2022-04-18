f9e23f2d4aea7afe853ac69699323aa245ab181c
EC2 Default User <ec2-user@ip-172-31-71-123.ec2.internal>
2022-04-18 17:08:58 +0000

    First Commit

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World from code-start',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
